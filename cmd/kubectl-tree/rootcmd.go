aa3d43296153e4f5c287bc4bd31020191eced54d
Shihyu Ho <methodho@gmail.com>
2022-09-30 11:09:32 +0800

    feat: accept KIND/NAME form

--- cmd/kubectl-tree/rootcmd.go
+++ cmd/kubectl-tree/rootcmd.go
@@ -47,7 +47,7 @@ var rootCmd = &cobra.Command{
 	Short:        "Show sub-resources of the Kubernetes object",
 	Example: "  kubectl tree deployment my-app\n" +
 		"  kubectl tree kservice.v1.serving.knative.dev my-app", // TODO add more examples about disambiguation etc
-	Args:    cobra.MinimumNArgs(2),
+	Args:    cobra.RangeArgs(1, 2),
 	RunE:    run,
 	Version: versionString(),
 }
@@ -90,7 +90,10 @@ func run(command *cobra.Command, args []string) error {
 	}
 	klog.V(3).Info("completed querying APIs list")
 
-	kind, name := args[0], args[1]
+	kind, name, err := figureOutKindName(args)
+	if err != nil {
+		return err
+	}
 	klog.V(3).Infof("parsed kind=%v name=%v", kind, name)
 
 	var api apiResource
