05fb999a64d5c68da4e22f4ec17025f6984ac42d
Bekir Dogan <bekirdo@gmail.com>
2023-12-09 18:50:36 +0000

    add support for showing kstatus (#66)

--- cmd/kubectl-tree/status.go
+++ cmd/kubectl-tree/status.go
@@ -5,45 +5,50 @@ import (
 
 	"k8s.io/apimachinery/pkg/apis/meta/v1/unstructured"
 	"k8s.io/klog"
+	"sigs.k8s.io/cli-utils/pkg/kstatus/status"
 )
 
 type ReadyStatus string // True False Unknown or ""
 type Reason string
 
-func extractStatus(obj unstructured.Unstructured) (ReadyStatus, Reason) {
+func extractStatus(obj unstructured.Unstructured) (ReadyStatus, Reason, status.Status) {
 	jsonVal, _ := json.Marshal(obj.Object["status"])
 	klog.V(6).Infof("status for object=%s/%s: %s", obj.GetKind(), obj.GetName(), string(jsonVal))
+	result, err := status.Compute(&obj)
+	if err != nil {
+		return "", "", ""
+	}
 	statusF, ok := obj.Object["status"]
 	if !ok {
-		return "", ""
+		return "", "", ""
 	}
 	statusV, ok := statusF.(map[string]interface{})
 	if !ok {
-		return "", ""
+		return "", "", ""
 	}
 	conditionsF, ok := statusV["conditions"]
 	if !ok {
-		return "", ""
+		return "", "", ""
 	}
 	conditionsV, ok := conditionsF.([]interface{})
 	if !ok {
-		return "", ""
+		return "", "", ""
 	}
 
 	for _, cond := range conditionsV {
 		condM, ok := cond.(map[string]interface{})
 		if !ok {
-			return "", ""
+			return "", "", ""
 		}
 		condType, ok := condM["type"].(string)
 		if !ok {
-			return "", ""
+			return "", "", ""
 		}
 		if condType == "Ready" {
 			condStatus, _ := condM["status"].(string)
 			condReason, _ := condM["reason"].(string)
-			return ReadyStatus(condStatus), Reason(condReason)
+			return ReadyStatus(condStatus), Reason(condReason), status.Status(result.Status.String())
 		}
 	}
-	return "", ""
+	return "", "", ""
 }
